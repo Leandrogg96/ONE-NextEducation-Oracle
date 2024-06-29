d34fd868745bd7c723e58e3abacb19b2b87911f8
Leandrogg96 <gallacleandrog@gmail.com>
2024-06-29 17:29:28 -0300

    Seccion 4.4 - Curso Logica de programacion

--- 1.1- Formacion - LogicaDeProgramacion/2034-logica-programacion-1-Aula1/app.js
+++ 1.1- Formacion - LogicaDeProgramacion/2034-logica-programacion-1-Aula1/app.js
@@ -1,17 +1,33 @@
-let numberUser = prompt("Can you give me a number between 0 and 20, please:");
-let secretNumber = 6;
+let secretNumber = 17;
+let numberUser = 0;
+let counterAttempt = 1;
+let maximumAttempt = 2;
 
+while (numberUser != secretNumber) {
+    numberUser = prompt("Can you give me a number between 0 and 20, please:");
 
-console.log(numberUser);
-
+    console.log(numberUser);
 /*
     The following code block performs the comparison
     between the entered number and the secret number
 */
-
-if (numberUser == secretNumber) {
-    alert(`You got the correct number. Is ${numberUser}!`);
-} else {
-    alert("Sorry, you get de incorrect number!");
+    if (numberUser == secretNumber) {
+        //if (counterAttempt == 1) {
+            alert(`You got the correct number. Is ${numberUser}! You did it in ${counterAttempt} ${counterAttempt == 1 ? 'trie.' : 'tries.'}`);
+        /*}else {
+            alert(`You got the correct number. Is ${numberUser}! You did it in ${counterAttempt} tries.`);
+        }*/
+    } else {
+        if (numberUser > secretNumber) {
+            alert("The secret number is less than.");
+        } else {
+            alert("The secret number is greater than.")
+        }
+        alert("Sorry, you get the incorrect number!");
+        if (counterAttempt > maximumAttempt) {
+            alert(`You reached the maximum number of attempts ${counterAttempt}. Whis is.`);
+            break;
+        }
+        counterAttempt++;
+    }
 }
-
