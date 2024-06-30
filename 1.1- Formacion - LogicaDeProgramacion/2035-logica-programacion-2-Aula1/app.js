caa87e12c3b3c620f8388b685be1e441c41f41b1
Leandrogg96 <gallacleandrog@gmail.com>
2024-06-30 20:24:11 -0300

    continuando curso 2 - logicaprogramacion

--- 1.1- Formacion - LogicaDeProgramacion/2035-logica-programacion-2-Aula1/app.js
+++ 1.1- Formacion - LogicaDeProgramacion/2035-logica-programacion-2-Aula1/app.js
@@ -1,9 +1,32 @@
+/*
 let title = document.querySelector('h1');
 title.innerHTML = 'Secret number game';
 
 let paragraph = document.querySelector('p');
 paragraph.innerHTML = 'Indicates a number from 1 to 10';
+*/
 
-function userAttempt() {
-    alert("Click on button.")
+function generateSecretNumber() {
+    return Math.floor(Math.random()*10)+1;
 }
+
+function assignElement(element, text) {
+    let elementHTML = document.querySelector(element);
+    elementHTML.innerHTML = text;
+    return;
+}
+
+function verifyAttempt() {
+    let numberUser = parseInt(document.getElementById('userValue').value);
+    console.log(numberUser);    
+    console.log(secretN1)
+    console.log(numberUser === secretN1);
+
+    return;
+}
+
+
+assignElement('h1', "Secret number game");
+assignElement('p', "Indicates a number from 1 to 10");
+
+let secretN1 = generateSecretNumber();
